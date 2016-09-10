669fdcf3a3b98bc1b84ea2434be56b26cc8f2047
Scott Frees <scott.frees@gmail.com>
2016-09-10 11:25:09 -0400

    angular controller

--- routes/registration.js
+++ routes/registration.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res) {
     // Check if a user is logged in, if so, redirect to the correct landing page - 
     // either participant portal or admin portal.
     // If not logged in, the landing page is the signin/signup page.
+    req.log.info("Displaying landing - user not logged in");
 	res.render("landing", {
 	  
     });
