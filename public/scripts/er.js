0caea3615703fc981d99a5c08e737fcfe5b693b7
Scott Frees <scott.frees@gmail.com>
2016-11-29 16:29:12 -0500

    help blocks

--- public/scripts/er.js
+++ public/scripts/er.js
@@ -7,9 +7,27 @@ var make_base_url = function($location) {
   return url;
 }
 
+var popover_content = function() {
+  var text = $(this).data("helptext");
+  if ( Array.isArray(text) ) {
+    var help_text = "";
+    for ( var i = 0; i < text.length; i++ ) {
+      help_text+="<p>" + text[i] + "</p>";
+    }
+    return help_text;
+  }
+  else {
+    return text;
+  }
+}
 
 $( document ).ready(function() {
     $(".switchBox").bootstrapSwitch();
     $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover({
+      content:popover_content
+    })
 });
+
+
 
