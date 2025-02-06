72946e1e801d545fee4b7e425724a6fa6cc1c3ae
Kristin Knight <kristin@kdahl.com>
2025-02-06 17:00:19 -0600

    provide debug information for api/brands endpoint to help figure out what keeps pinging the app

--- routes/ratings.js
+++ routes/ratings.js
@@ -58,9 +58,24 @@ router.get('/api/participants', function (req, res) {
     });
 });
 
+// KK HERE
 router.get('/api/brands', aw(async (req, res) => {
     var name = req.query.name;
     var query = {};
+    const { rawHeaders, httpVersion, method, socket, url } = req;
+    const { remoteAddress, remoteFamily } = socket;
+  
+    console.log(
+      JSON.stringify({
+        timestamp: Date.now(),
+        rawHeaders,
+        httpVersion,
+        method,
+        remoteAddress,
+        remoteFamily,
+        url
+      })
+    );
 
     if (name) {
         const participant = await req.Participants.findOne({
