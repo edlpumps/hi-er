adb2332be8088b411a3f8052f03aeb3ca9fc728a
Kristin Knight <kristin@kdahl.com>
2026-03-27 15:18:04 -0500

    Add database connected to to Beta/Dev button

--- index.js
+++ index.js
@@ -136,6 +136,7 @@ var configure = function () {
         res.locals.label_lang = lang.get_label_language();
         res.locals.page_lang = lang.get_page_language();
         res.locals.is_beta = process.env.NODE_ENV ? process.env.NODE_ENV.toUpperCase() : undefined;
+        res.locals.db_connection = data_connection_str.split('?')[0].split('27017/').at(-1);
 
         res.locals.moment = require('moment');
         next();
