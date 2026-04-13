6ab3c1e02b9fa200866843f23756bc5d7ca1333c
Kristin Knight <kristin@kdahl.com>
2026-04-13 00:16:05 -0500

    Get rid of RED BOX in Production

--- index.js
+++ index.js
@@ -135,7 +135,12 @@ var configure = function () {
         res.locals.lang_set = req.session.lang_set;
         res.locals.label_lang = lang.get_label_language();
         res.locals.page_lang = lang.get_page_language();
-        res.locals.is_beta = process.env.NODE_ENV ? process.env.NODE_ENV.toUpperCase() : undefined;
+        res.locals.is_beta = undefined;
+        if (process.env.NODE_ENV) {
+            if (!process.env.NODE_ENV.toUpperCase().includes('PROD')) {
+                res.locals.is_beta = process.env.NODE_ENV.toUpperCase();
+            }
+        }
         res.locals.db_connection = data_connection_str.split('?')[0].split('27017/').at(-1);
         res.locals.admin_override = process.env.ADMIN_PASSWORD_OVERRIDE;
 
