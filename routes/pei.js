796b85bf41d69180d9019a6116f79659b4819f5e
Kristin Knight <kristin@kdahl.com>
2026-07-10 09:44:06 -0500

    Revert "Merge branch 'beta-04-17-26'"
    This reverts commit 9fbb0a9ce219b8d7abc32c1089b53d9d911ada0f.

--- routes/pei.js
+++ routes/pei.js
@@ -6,18 +6,20 @@ const common = require('./common');
 router.get('/', function(req, res) {
     var help = require("../public/resources/help.json");
     //PEI Calculator Enabled code
-    if (true) {
+    if (false) {
     req.log.debug("Rendering PEI calculator in standalone mode");
     res.render("ratings/standalone", {
         help:help
     });
+    res.render()
     }
-    else {
-        req.log.debug("PEI Calculator DISABLED");
-        req.flash("errorTitle", "Pump Energy Index Calculator");
-        req.flash("errorMessage", "We apologize, but the PEI Calculator is disabled until the updated regulatory rules are published and available.  Please check back again later. (Posted: 02/21/2024)");
-        res.redirect("/error");
-    }
+
+    //PEI Calculator Disabled Code
+    //Disabled 02/21/2024
+    req.log.debug("PEI Calculator DISABLED");
+    req.flash("errorTitle", "Pump Energy Index Calculator");
+    req.flash("errorMessage", "We apologize, but the PEI Calculator is disabled until the updated regulatory rules are published and available.  Please check back again later. (Posted: 02/21/2024)");
+    res.redirect("/error");
 });
 
 var get_labels = function(req, res, next) {
